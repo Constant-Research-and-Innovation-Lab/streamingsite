3b266c825982b10245adeba115bcad6e854a160d
ardhrubo <ardhrubo3038@gmail.com>
2024-03-13 14:59:18 +0600

    middlewares

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
-let numbers = [2, 4, 8];
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-let answer = numbers.reduceRight((x, y) => x / y, 1);
+const app = express()
 
-console.log(answer);
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+export { app };
