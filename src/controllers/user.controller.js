7ed792a99d0388589872b1581cafe9152c33d4ca
ardhrubo <ardhrubo3038@gmail.com>
2024-03-18 23:43:06 +0600

    new info

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,13 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        messages: " Chill it is working"
-    })
+    // get user detailes from frontend
+    // validation -not empty
+    // check if user already exists: username,email
+    // check for images , check for avatar
+    // upload them to cloudinary
+
+
 })
 
 
