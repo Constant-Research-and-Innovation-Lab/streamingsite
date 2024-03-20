f3e73e45bb06e415c3cbdd69ec4ba15f247dbc12
ardhrubo <ardhrubo3038@gmail.com>
2024-03-20 17:43:33 +0600

    logout user validation

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,7 +1,16 @@
+import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
 
 
 
 export const verifyJWT = asyncHandler(async(req,res,next)=>{
-    req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+    if(!token){
+        throw new ApiError(401,"Unauthorized request")
+    }
+
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+
+
 })
