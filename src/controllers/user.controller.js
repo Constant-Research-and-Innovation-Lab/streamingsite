20c97de974f7ebf4dec55ec9e0dd0d1135e81e38
ardhrubo <ardhrubo3038@gmail.com>
2024-03-23 15:58:37 +0600

    rffix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -194,7 +194,7 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
             throw new ApiError(401,"Invalid refresh token")
         }
     
-        if(incomingRefreshToken !== user?.refreshToken){
+        if(!incomingRefreshToken !== user?.refreshToken){
             throw new ApiError(401, "Refresh token is expired or used")
         }
     
@@ -230,6 +230,7 @@ export {
     
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 
 };
